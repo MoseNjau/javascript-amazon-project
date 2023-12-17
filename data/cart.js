9daaebc60086e4e0241d4e7bc0f4b02ad327fd83
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-12-17 14:48:57 +0300

    updated the modules

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,20 @@
-    const cart = []
+export const cart = []
+
+export function addToCart(productId) {
+    let matchingItem;
+    
+    cart.forEach((CartItem) => {
+        if (productId === CartItem.productId) {
+            matchingItem = CartItem;
+        }
+    });
+    
+    if (matchingItem) {
+        matchingItem.quantity += 1;
+    } else {
+        cart.push({
+            productId: productId,
+            quantity: 1
+        });
+    }
+  }
--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,6 @@
+import { cart, addToCart } from '../data/cart.js';
+import { products } from '../data/products.js';
+
 let productsHTML = '';
 products.forEach((product)=>{
     productsHTML += `
@@ -57,37 +60,22 @@ products.forEach((product)=>{
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
+function updateCartQuantity() {
+  let cartQuantity = 0;
+  
+  cart.forEach((CartItem) => {
+    cartQuantity += CartItem.quantity;
+  })
+  
+  document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+}
+
 document.querySelectorAll('.js-add-to-cart')
     .forEach((button) => {
         button.addEventListener('click', () => {
-            productId = button.dataset.productId;
-
-            let matchingItem;
-
-            cart.forEach((item) => {
-                if (productId === item.productId) {
-                    matchingItem = item;
-                }
-            });
-
-            if (matchingItem) {
-                matchingItem.quantity += 1;
-            } else {
-                cart.push({
-                    productId: productId,
-                    quantity: 1
-                });
-            }
-
-            let cartQuantity = 0;
-
-            cart.forEach((item) => {
-              cartQuantity += item.quantity;
-            })
-
-            document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-
-            
+            const productId = button.dataset.productId;
+            addToCart(productId);
+            updateCartQuantity();
         });
     });
 
