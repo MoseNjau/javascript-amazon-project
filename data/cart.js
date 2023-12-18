dfb44f365f0118464f4b23b5d52f428d92f413dd
MoseNjau <moses.njau@students.jkuat.ac.ke>
2023-12-18 13:28:57 +0300

    made the ecommerce interactive

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,19 @@
-export const cart = []
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = [{
+        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 2
+    },
+    {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+}
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -17,4 +32,18 @@ export function addToCart(productId) {
             quantity: 1
         });
     }
+    saveToStorage();
+  }
+
+export function removeFromCart(productId) {
+    const newCart = [];
+
+    cart.forEach((CartItem) => {
+        if (CartItem.productId !== productId) {
+            newCart.push(CartItem);
+        }
+    });
+
+    cart= newCart;
+    saveToStorage();
   }
