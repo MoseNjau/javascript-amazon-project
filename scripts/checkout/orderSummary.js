35e80da9d807f9c175eef3ec8e319866a8161a11
MoseNjau <moses.njau@students.jkuat.ac.ke>
2024-01-14 09:07:06 +0300

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,11 +4,7 @@ import { formatCurrency } from '../utils/money.js';
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'
 import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
-hello();
-
-const today = dayjs();
-const deliveryDate = today.add(7, 'day');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
   let cartSummaryHTML = '';
@@ -124,6 +120,7 @@ export function renderOrderSummary(){
         container.remove();
   
         updateCartQuantity();
+        renderPaymentSummary();
       })
     })
   
@@ -166,6 +163,7 @@ export function renderOrderSummary(){
           quantityLabel.innerHTML = newQuantity;
   
           updateCartQuantity();
+          renderPaymentSummary();
         });
       });
   
@@ -175,6 +173,7 @@ export function renderOrderSummary(){
         const {productId, deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId)
         renderOrderSummary();
+        renderPaymentSummary();
         //location.reload()
       });
     });
